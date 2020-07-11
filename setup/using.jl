8a03d6197b2184be5aca932534c2abff7b3d189c
BradenDKelly <bradenzkelly@gmail.com>
2020-07-11 02:49:29 -0400

    added import file and empty pressure file

---
+++ setup/using.jl
@@ -0,0 +1,15 @@
+# All the modules that need to be present
+
+import Pkg;
+Pkg.add("Distributions")
+Pkg.add("Random")
+Pkg.add("LinearAlgebra")
+Pkg.add("BenchmarkTools")
+Pkg.add("Setfield")
+Pkg.add("StaticArrays")
+Pkg.add("StructArrays")
+Pkg.add("SpecialFunctions")
+Pkg.add("OffsetArrays")
+Pkg.add("Printf")
+Pkg.add("Dates")
+Pkg.add("Profile")
